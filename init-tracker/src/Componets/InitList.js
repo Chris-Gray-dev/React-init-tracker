607f6c26487387788550b7ee9b2123c5ed97810a
Chris Gray <combly123@gmail.com>
2020-04-05 12:48:40 -0400

    added drag and not quite drop

--- init-tracker/src/Componets/InitList.js
+++ init-tracker/src/Componets/InitList.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import InitListItem from './InitListItem'
+import {DragDropContext,Droppable} from "react-beautiful-dnd"
+
+const data=[
+    {id:"1",text:"Monster one"},
+    {id:"2",text:"Monster two"},
+    {id:"3",text:"Monster three"},
+    {id:"4",text:"Player one"},
+]
+
+
+
+
+
+// Main component
 function InitList()
 {
+    // helper functions
+    function RenderInitItems(arr){
+        let array = arr.map((item,index)=><InitListItem key={item.id} info={item} index={index}/>)
+        return array
+    }
 
+    function onDragEnd(result){
+
+    }
+
+    const [list,setList] = useState(data);
+    const id = '1';
 
     return(
-        <div className = "Init-List">
-            <InitListItem />
-            <InitListItem />
-            <InitListItem />   
-        </div>
+        <DragDropContext>
+            <Droppable droppableId={id}>
+                {provided=>(
+                    <div ref={provided.innerRef} {...provided.droppableProps} className = "Init-List">
+                        {RenderInitItems(list)}
+                        {provided.placeholder}
+                    </div>
+                )}
+                
+            </Droppable>           
+        </DragDropContext> 
     )
 }
 
--- init-tracker/src/Componets/InitListItem.js
+++ init-tracker/src/Componets/InitListItem.js
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import {Draggable} from "react-beautiful-dnd"
 
-function InitListItem()
+
+
+function InitListItem(props)
 {
     var randomColor = require('randomcolor'); // import the script
-    var color = randomColor(); // a hex code for an attractive color
+    const [color,setColor] = useState("")
 
+    
     let Style ={
             borderLeft:`solid ${color} 10px`,
             padding:10,
             margin:10,
             borderTop:'solid black 1px',
             borderBottom:'solid black 1px',
-            borderRight:'solid black 1px'
+            borderRight:'solid black 1px',
+            background:"white"
     }
+    
+    useEffect(()=>{
+            setColor(randomColor())
+        },[])
+
+
 
     return(
-        <div style={Style}>
-            <h1>Line 1</h1>
-            <h1>Line 2</h1>            
-        </div>
+        <Draggable draggableId={props.info.id} index={props.index}>
+            {
+                (provided)=>(
+                    <div  {...provided.dragHandleProps} {...provided.draggableProps} ref={provided.innerRef}>
+                        <div style={Style}>
+                            <h1>{props.info.id}</h1>
+                            <h1>{props.info.text}</h1>  
+                        </div>                                  
+                    </div>
+                )
+            }
+            
+        </Draggable>
     )
 }
 
